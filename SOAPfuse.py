6d4bc0c6f61e4b603c367f87decd38cd83c49310
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2020-06-30 08:58:50 +0900

    Update SOAPfuse.py

--- SOAPfuse.py
+++ SOAPfuse.py
@@ -1,14 +1,7 @@
 import os
 
-'''
-sample='LPS3'
-sample_list='LPS3_list.txt'
-output_dir='/home/DATA/Drug/RNA-seq/LPS3_SOAPfuse/'
-os.system('perl /home/program/SOAPfuse-v1.27/SOAPfuse-RUN.pl -c /home/program/SOAPfuse-v1.27/config/config.txt -fd '+sample+' -l '+sample_list+' -o '+output_dir+' -tp '+sample)
-'''
-
-sample_name='Poly4'
-sample_dir='/home/DATA/Drug/RNA-seq/Poly4/'
-sample_list='/home/DATA/Drug/RNA-seq/SOAPfuse/Poly4_list.txt'
-output_dir='/home/DATA/Drug/RNA-seq/SOAPfuse/Poly4_SOAPfuse/'
+sample_name='sample'
+sample_dir='/home/DATA/Drug/RNA-seq/sample/'
+sample_list='/home/DATA/Drug/RNA-seq/SOAPfuse/sample_list.txt'
+output_dir='/home/DATA/Drug/RNA-seq/SOAPfuse/sample_SOAPfuse/'
 os.system('perl /home/program/SOAPfuse-v1.27/SOAPfuse-RUN.pl -c /home/program/SOAPfuse-v1.27/config/config.txt -fd '+sample_dir+' -l '+sample_list+' -o '+output_dir+' -tp '+sample_name)
