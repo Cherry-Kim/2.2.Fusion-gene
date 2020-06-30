6895e77d98bae9497467870603a08047e82c2a4d
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2020-06-30 09:03:25 +0900

    Update SOAPfuse.py

--- SOAPfuse.py
+++ SOAPfuse.py
@@ -4,4 +4,5 @@ sample_name='sample'
 sample_dir='/home/DATA/RNA-seq/sample/'
 sample_list='/home/DATA/RNA-seq/SOAPfuse/sample_list.txt'
 output_dir='/home/DATA/RNA-seq/SOAPfuse/sample_SOAPfuse/'
+
 os.system('perl SOAPfuse-v1.27/SOAPfuse-RUN.pl -c SOAPfuse-v1.27/config/config.txt -fd '+sample_dir+' -l '+sample_list+' -o '+output_dir+' -tp '+sample_name)
