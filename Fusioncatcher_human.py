ebbd736282814c460c0d550626e653331c0becb1
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2020-07-08 09:45:31 +0900

    Update Fusioncatcher_human.py

--- Fusioncatcher_human.py
+++ Fusioncatcher_human.py
@@ -1,8 +1,8 @@
 import string,os
 
 print "Case-Control"
-sample1='/home/DATA/Drug/RNA-seq/Sample_MEF1/'
-sample2='/home/DATA/Drug/RNA-seq/Sample_RAW_LPS3/'
-output='/home/DATA/Drug/RNA-seq/LPS_MEF1_results/'
+sample1='/home/RNA-seq/sample1/'
+sample2='/home/RNA-seq/sample2/'
+output='/home/RNA-seq/sample1_sample2/'
 
 os.system('/home/program/fusioncatcher/bin/fusioncatcher.py --input '+sample1+' --normal '+sample2+' --output '+output+' -p 24')
